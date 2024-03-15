f4f055eca8b90d4a947db19a2fba9b4bdb9d2bfa
Seoyeon-park0103 <162958493+Seoyeon-park0103@users.noreply.github.com>
2024-03-15 19:16:22 +0900

    Add files via upload

---
+++ 백준.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+u=[5,9,6]
+fig=plt.figure()
+ax=plt.axes(projection='3d')
+ax.set_xlim(-1,10)
+ax.set_ylim(-10,10)
+ax.set_xlim(0,10)
+start=[0,0,0]
+ax.quiver(start[0],start[1],start[2],u[0],u[1],u[2],color='r')
